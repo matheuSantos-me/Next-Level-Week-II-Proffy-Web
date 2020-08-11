ae3632cd0003b576d5223002856a72323a0d015d
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-08-11 00:18:47 -0300

    docs: Extract components

--- src/components/CardTeacher/index.tsx
+++ src/components/CardTeacher/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import WhatsApp from '../../assets/images/icons/whatsapp.svg'
+
 const CardTeacher = () => {
 
   return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars2.githubusercontent.com/u/59510927?s=460&u=3c88c79aebe9a224c23bddddcb2465323ce8d1bf&v=4" alt="AAAAAAAAAAAAAA" />
 
+        <div>
+          <strong>AAAAAAAAAAAAAAAAAAAAAAaaa</strong>
+          <strong>oiiiiiiiiiiiiiiiiiiiii</strong>
+        </div>
+      </header>
+
+      <p>
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa
+        <br /> <br />
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa
+      </p>
+
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ 80,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={WhatsApp} alt="" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
   )
 }
 
