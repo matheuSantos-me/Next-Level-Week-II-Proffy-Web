d976191fb0b2dbbc98335889fc9ced9d37ae4b8c
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-08-11 23:42:20 -0300

    docs: Create layout finish

--- src/Pages/FormTeacher/index.tsx
+++ src/Pages/FormTeacher/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from '../../components/Header'
 import Input from '../../components/Input'
@@ -10,6 +10,14 @@ import Warning from '../../assets/images/icons/warning.svg'
 import './styles.css'
 
 const FormTeacher = () => {
+  const [scheduleItem, setScheduleItem] = useState([{ week_day: 0, from: '', to: '' }])
+
+  const addNewSchedule = () => {
+    setScheduleItem([
+      ...scheduleItem,
+      { week_day: 0, from: '', to: '' }
+    ])
+  }
 
   return (
     <div id="page-teacher-form" className="container">
@@ -48,12 +56,37 @@ const FormTeacher = () => {
         </fieldset>
 
         <fieldset>
-          <legend>Horários disponíveis <button type="button">+ Novo horário</button></legend>
+          <legend>Horários disponíveis <button type="button" onClick={addNewSchedule}>+ Novo horário</button></legend>
+
+          {
+            scheduleItem.map(item => {
+              return (
+                <div key={item.week_day} className="schedule-item">
+                  <Select
+                    name="week_day"
+                    label="Dia da semana"
+                    options={[
+                      { value: "0", label: "Domingo" },
+                      { value: "1", label: "Segunda-feira" },
+                      { value: "2", label: "Terça-feira" },
+                      { value: "3", label: "Quarta-feira" },
+                      { value: "4", label: "Quinta-feira" },
+                      { value: "5", label: "Sexta-feira" },
+                      { value: "6", label: "Sábado" }
+                    ]}
+                  />
+
+                  <Input type="time" name="from" label="Das" />
+
+                  <Input type="time" name="to" label="Até" />
+                </div>
+              )
+            })
+          }
         </fieldset>
 
         <footer>
           <p><img src={Warning} alt="Aviso importante" /> Importante! <br /> Preencha todos os dados</p>
-
           <button>Savar cadastro</button>
         </footer>
       </main>
