f51c09a459dbb9b433939fabd8e3a4cc38d705aa
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-08-20 21:50:50 -0300

    fix: Layout

---
+++ src/Pages/TeacherList/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import PageHeader from '../../components/Molecule/PageHeader'
+import TeacherItem from '../../components/Molecule/TeacherItem'
+import Select from '../../components/Atom/Select'
+import Input from '../../components/Atom/Input'
+
+import './styles.css'
+
+const TeacherList = () => {
+
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+        <Select 
+            name="subject" 
+            label="Matéria" 
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Filosofia', label: 'Filosofia' },
+              { value: 'Sociologia', label: 'Sociologia' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Espanhol', label: 'Espanhol' },
+              { value: 'Língua Portuguesa', label: 'Língua Portuguesa' },
+              { value: 'Literatura', label: 'Literatura' }
+            ]}
+          />
+          <Select 
+            name="week_day" 
+            label="Dia da semana" 
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-Feira' },
+              { value: '2', label: 'Terça-Feira' },
+              { value: '3', label: 'Quarta-Feira' },
+              { value: '4', label: 'Quinta-Feira' },
+              { value: '5', label: 'Sexta-Feira' },
+              { value: '6', label: 'Sábado' }
+            ]}
+          />
+          <Input type="time" name="time" label="Hora" />
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  )
+}
+
+export default TeacherList
